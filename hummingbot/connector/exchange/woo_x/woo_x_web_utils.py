5f16ac00c200cf77eb811c26f4e472147f0c6615
waterquarks <waterquarks@gmail.com>
2023-06-07 22:26:33 +0000

    First commit

---
+++ hummingbot/connector/exchange/woo_x/woo_x_web_utils.py
@@ -0,0 +1,38 @@
+from typing import Callable, Optional
+
+import hummingbot.connector.exchange.woo_x.woo_x_constants as CONSTANTS
+from hummingbot.connector.time_synchronizer import TimeSynchronizer
+from hummingbot.core.api_throttler.async_throttler import AsyncThrottler
+from hummingbot.core.web_assistant.auth import AuthBase
+from hummingbot.core.web_assistant.web_assistants_factory import WebAssistantsFactory
+
+
+def rest_url(path_url: str, domain: str = CONSTANTS.DEFAULT_DOMAIN) -> str:
+    """
+    Creates a full URL for provided public REST endpoint
+    :param path_url: a public REST endpoint
+    :param domain: the Woo X domain to connect to ("com" or "us"). The default value is "com"
+    :return: the full URL to the endpoint
+    """
+    return CONSTANTS.REST_URLS[domain] + path_url
+
+
+def build_api_factory(
+    throttler: Optional[AsyncThrottler] = None,
+    time_synchronizer: Optional[TimeSynchronizer] = None,
+    domain: str = CONSTANTS.DEFAULT_DOMAIN,
+    time_provider: Optional[Callable] = None,
+    auth: Optional[AuthBase] = None
+) -> WebAssistantsFactory:
+    throttler = throttler or create_throttler()
+
+    api_factory = WebAssistantsFactory(
+        throttler=throttler,
+        auth=auth
+    )
+
+    return api_factory
+
+
+def create_throttler() -> AsyncThrottler:
+    return AsyncThrottler(CONSTANTS.RATE_LIMITS)
